a9174853c4086bd7feb4d5268cadbf1a5adf3ab9
Muhammad Azeem <mazeem182000@gmail.com>
2024-03-11 03:18:53 +0500

    first commit

--- server/router/DataRouter.js
+++ server/router/DataRouter.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+
 const {
   postNewFormData,
   getCityInfo,
 } = require("../controller/dataController");
 const router = express.Router();
 
-//
-const storage = multer.memoryStorage();
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
 const upload = multer({ storage });
 
 router.route("/formdatapost").post(upload.single("image"), postNewFormData);
